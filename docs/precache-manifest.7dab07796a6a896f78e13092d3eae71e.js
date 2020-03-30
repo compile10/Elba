2ed453cd4d2b3f750180053503de4fd5ada14136
compile10 <efrain800@gmail.com>
2020-03-30 00:10:57 -0700

    added docs folder

---
+++ docs/precache-manifest.7dab07796a6a896f78e13092d3eae71e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9ed610acacd77a44aa5c539671bc04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6eeae6419c9cac637ae",
+    "url": "/static/css/main.72c039aa.chunk.css"
+  },
+  {
+    "revision": "b4aab090adfe11acfbb1",
+    "url": "/static/js/2.7f9bd4ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7f9bd4ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6eeae6419c9cac637ae",
+    "url": "/static/js/main.9e6bef86.chunk.js"
+  },
+  {
+    "revision": "60041a895665412c6a6d",
+    "url": "/static/js/runtime-main.0854a828.js"
+  }
+]);
